67e4fd51911e12a25e7351b0593a00e703661489
evkondr <kondratiev.evg@gmail.com>
2018-11-15 21:21:42 +0300

    nav has been updated

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 window.onload=function(){
-    document.getElementById("burg").onclick=function(){
-        document.getElementsByClassName("items")[0].classList.toggle("active");
+    var menu=document.getElementsByClassName("items")[0];
+    document.getElementsByClassName("hamburger")[0].onclick=function(){
+        this.classList.toggle("is-active");
+        menu.classList.toggle("active");
     }
 }
